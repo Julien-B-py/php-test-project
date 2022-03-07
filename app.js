b75e7b97c1b790661766755c70d18890cb30a720
Julien-B-py <86344743+Julien-B-py@users.noreply.github.com>
2022-03-07 08:02:03 +0100

    v0.1.1
    Edit image animations.
    Edit reloadPage function.
    Turn fetch into an independent function.

--- app.js
+++ app.js
@@ -1,34 +1,45 @@
 // Store <img> in a variable
 const img = document.querySelector("img");
 
-fetch("http://localhost:8000/getRandomImg.php").then(res => {
+const getImg = async () => {
 
-    // If response successful
-    if (res.ok) {
-        res.json().then(receivedImgUrl => {
-            // set image src attribute to be equal to the img url received from the server
-            img.src = receivedImgUrl;
-        });
-        // If error in request
-    } else {
-        // Display error image
-        img.src = "/no_img.png";
-    }
+    await fetch("http://localhost:8000/getRandomImg.php").then((res) => {
+        // If response successful
+        if (res.ok) {
+            res.json().then((receivedImgUrl) => {
+                // set image src attribute to be equal to the img url received from the server
+                img.src = receivedImgUrl;
+            });
+            // If error in request
+        } else {
+            // Display error image
+            img.src = "/no_img.png";
+        }
+    });
 
-});
+};
 
 // When image is loaded
-img.addEventListener("load", event => {
+img.addEventListener("load", (event) => {
+
     // Animate image scale from 0 to 1 then call reloadPage.
-    gsap.to("img", { scale: 1, duration: 1, ease: Bounce.easeOut, onComplete: () => reloadPage() });
+    gsap.fromTo("img", { scale: 0 }, {
+        scale: 1,
+        duration: 1,
+        ease: Bounce.easeOut,
+        onComplete: () => reloadImg(),
+    });
 
 });
 
 // Reload page after 3sec when animation is completed
-const reloadPage = () => {
+const reloadImg = () => {
 
     setTimeout(() => {
-        location.reload();
+        getImg();
     }, 3000);
-
+    
 };
+
+// Run 
+getImg();
